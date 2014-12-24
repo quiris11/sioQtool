fbe78fc8492b2609f0e80872c4912b27442940ab
Robert Błaut <listy@blaut.biz>
2014-12-24 11:44:30 +0100

    dictionaries in separate file

---
+++ dictionaries.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# This file is part of sioQtool, licensed under GNU Affero GPLv3 or later.
+# Copyright © Robert Błaut. See NOTICE for more information.
+#
+
+kat_ucz_dict = {
+    1: 'Dzieci lub młodzież',
+    2: 'Dorośli',
+    3: 'Bez kategorii',
+}
+
+publ_dict = {
+    1: 'publiczna',
+    2: 'niepubliczna o uprawnieniach szkoły publicznej',
+    3: 'niepubliczna bez uprawnień szkoły publicznej',
+    4: 'niepubliczna'
+}
+
+type_dict = {
+    1: 'Przedszkole',
+    3: 'Szkoła podstawowa',
+    4: 'Gimnazjum',
+    13: 'Zasadnicza szkoła zawodowa',
+    14: 'Liceum ogólnokształcące',
+    15: 'Liceum profilowane',
+    16: 'Technikum',
+    17: 'Liceum ogólnokształcące uzupełniające dla absolwentów '
+        'zasadniczych szkół zawodowych',
+    18: 'Technikum uzupełniające dla absolwentów zasadniczych szkół '
+        'zawodowych',
+    19: 'Szkoła policealna (ponadgimnazjalna)',
+    20: 'Szkoła specjalna przysposabiająca do pracy dla uczniów z '
+        'upośledzeniem umysłowym w stopniu umiarkowanym lub znacznym oraz '
+        'dla uczniów z  więcej niż jedną niepełnosprawnością',
+    21: 'Sześcioletnia ogólnokształcąca szkoła muzyczna I stopnia',
+    22: 'Sześcioletnia szkoła muzyczna I stopnia',
+    23: 'Czteroletnia szkoła muzyczna I stopnia',
+    24: 'Sześcioletnia ogólnokształcąca szkoła muzyczna II stopnia',
+    25: 'Sześcioletnia szkoła muzyczna II stopnia',
+    26: 'Sześcioletnia ogólnokształcąca szkoła sztuk pięknych',
+    27: 'Czteroletnie liceum plastyczne',
+    29: 'Dziewięcioletnia ogólnokształcąca szkoła baletowa',
+    30: 'Sześcioletnia szkoła sztuki tańca',
+    31: 'Czteroletnia szkoła sztuki cyrkowej',
+    32: 'Policealna szkoła artystyczna',
+    33: 'Szkoła pomaturalna bibliotekarska',
+    34: 'Kolegium nauczycielskie',
+    35: 'Nauczycielskie Kolegium Języków Obcych',
+    36: 'Ośrodek politechniczny',
+    37: 'Pałac młodzieży',
+    38: 'Młodzieżowy dom kultury',
+    39: 'Ognisko pracy pozaszkolnej',
+    40: 'Międzyszkolny ośrodek sportowy',
+    41: 'Ogród jordanowski',
+    42: 'Pozaszkolna placówka specjalistyczna',
+    43: 'Szkolne schronisko młodzieżowe',
+    44: 'Placówki artystyczne (ognisko artystyczne)',
+    45: 'Centrum Kształcenia Praktycznego',
+    46: 'Centrum Kształcenia Ustawicznego ze szkołami',
+    47: 'Ośrodek dokształcania i doskonalenia zawodowego',
+    48: 'Poradnia psychologiczno-pedagogiczna',
+    49: 'Poradnia specjalistyczna',
+    50: 'Specjalny Ośrodek Wychowawczy',
+    51: 'Specjalny Ośrodek Szkolno-Wychowawczy',
+    52: 'Ośrodek Rewalidacyjno-Wychowawczy umożliwiający upośledzonym umysłowo'
+        ' realizację obowiązku szkolnego i nauki',
+    53: 'Młodzieżowy Ośrodek Wychowawczy',
+    54: 'Młodzieżowy Ośrodek Socjoterapii ze szkołami',
+    55: 'Bursa',
+    56: 'Dom wczasów dziecięcych',
+    57: 'Placówka doskonalenia nauczycieli',
+    58: 'Biblioteki pedagogiczne',
+    59: 'Publiczna placówka opiekuńczo-wychowawcza w systemie pomocy '
+        'społecznej',
+    60: 'Zakład poprawczy',
+    61: 'Schronisko dla nieletnich',
+    62: 'Rodzinny ośrodek diagnostyczno-konsultacyjny',
+    63: 'Publiczny ośrodek adopcyjno-opiekuńczy',
+    64: 'Niepubliczna placówka oświatowo-wychowawcza w systemie oświaty',
+    65: 'Kolegium Pracowników Służb Społecznych',
+    66: 'Szkoła pomaturalna animatorów kultury',
+    67: 'Delegatura',
+    68: 'Internat',
+    69: 'Czteroletnia szkoła muzyczna II stopnia',
+    70: 'Dziewięcioletnia szkoła sztuki tańca',
+    73: 'Szkoła specjalna przysposabiająca do pracy na podbudowie 8-letniej '
+        'szkoły podstawowej',
+    74: 'Centrum Kształcenia Ustawicznego - bez szkół',
+    75: 'Niepubliczna placówka kształcenia ustawicznego i praktycznego',
+    76: 'Młodzieżowy Ośrodek Socjoterapii bez szkół',
+    80: 'Zespół wychowania przedszkolnego',
+    81: 'Punkt przedszkolny',
+    82: 'Poznańska szkoła chóralna ',
+    83: 'Niepubliczna placówka kształcenia ustawicznego i praktycznego ze '
+        'szkołami',
+    100: 'Zespół szkół i placówek oświatowych'
+}
