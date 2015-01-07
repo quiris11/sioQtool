1ff264a51ca0844d12dcbff4018e3fbee29cc1e6
Robert Błaut <listy@blaut.biz>
2015-01-07 08:15:17 +0100

    bash script downloading required new sio reports

---
+++ wg-raports.sh
@@ -0,0 +1,9 @@
+#!/bin/bash 
+read -p "Username: " UNAME
+read -s -p "Password: " PASSWD
+wget --delete-after --keep-session-cookies --save-cookies=my_cookies.txt --post-data="nazwaUzytkownika=$UNAME&hasloUzytkownika=$PASSWD&param=Start_login" https://sio.men.gov.pl/dodatki/strefa/index.php
+wget -N --load-cookies=my_cookies.txt https://sio.men.gov.pl/dodatki/strefa/index.php?param=Support_download_6 --output-document=$HOME/NSIO/rspo_active.xls
+wget -N --load-cookies=my_cookies.txt https://sio.men.gov.pl/dodatki/strefa/index.php?param=Support_download_65 --output-document=$HOME/NSIO/rspo_inactive.xls
+wget -N --load-cookies=my_cookies.txt https://sio.men.gov.pl/dodatki/strefa/index.php?param=Support_download_61 --output-document=$HOME/NSIO/ee_przedszk.xls
+wget -N --load-cookies=my_cookies.txt https://sio.men.gov.pl/dodatki/strefa/index.php?param=Support_download_62 --output-document=$HOME/NSIO/ee_sp.xls
+rm my_cookies.txt
