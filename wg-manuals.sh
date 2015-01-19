10108979d5a86b0a04f717bfe7abd3a2ec8d04de
Robert Błaut <listy@blaut.biz>
2015-01-19 11:24:26 +0100

    more manuals

--- wg-manuals.sh
+++ wg-manuals.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 rm -r ~/cie_pdf/*.pdf
 wget --no-clobber --convert-links -r -p -E -e robots=off -U mozilla -A pdf -nd -P ~/cie_pdf http://sio.men.gov.pl/index.php/pomoc/instrukcje-uzytkownika/251-instrukcje-techniczne-do-obslugi-programu 
+wget --no-clobber --convert-links -r -p -E -e robots=off -U mozilla -A pdf -nd -P ~/cie_pdf http://sio.men.gov.pl/index.php/pomoc/instrukcje-uzytkownika/357-strefa-dla-zalogowanych
+wget --no-clobber --convert-links -r -p -E -e robots=off -U mozilla -A pdf -nd -P ~/cie_pdf http://sio.men.gov.pl/index.php/pomoc/instrukcje-uzytkownika/245-komentarze-merytoryczne-do-instrukcji-uzytkownika
