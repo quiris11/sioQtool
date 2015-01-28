0bb11611820629e7072470584dd2039be4fc1bd2
Robert Błaut <listy@blaut.biz>
2015-01-28 20:02:10 +0100

    removed wget dependency

--- tools/py-reports.py
+++ tools/py-reports.py
@@ -10,11 +10,9 @@ import shutil
 import getpass
 from os.path import expanduser
 from lxml import etree
-import subprocess
 import urllib
 import urllib2
-import sys
-import re
+from cookielib import CookieJar
 
 
 home = expanduser("~")
@@ -32,7 +30,6 @@ report_list = [
     ['64', 'zawody.xls']
 ]
 
-fnull = open(os.devnull, 'w')
 uname = raw_input("Username: ")
 passwd = getpass.getpass()
 
@@ -42,23 +39,9 @@ data = urllib.urlencode({
     'hasloUzytkownika': passwd,
     'param': 'Start_login'
 })
-req = urllib2.Request(url, data)
-response = urllib2.urlopen(req)
-cookie = response.info()['Set-Cookie']
-
-if cookie:
-    print(cookie)
-
-# sys.exit()
-subprocess.check_call([
-    'wget',
-    '--delete-after',
-    '--keep-session-cookies',
-    '--save-cookies=my_cookies.txt',
-    '--post-data=nazwaUzytkownika=%s&hasloUzytkownika=%s&param='
-    'Start_login' % (uname, passwd),
-    'https://sio.men.gov.pl/dodatki/strefa/index.php'
-], stdout=fnull, stderr=subprocess.STDOUT)
+cj = CookieJar()
+opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
+response = opener.open(url, data)
 
 for i in report_list:
     try:
@@ -68,23 +51,14 @@ for i in report_list:
     except:
         print('Error! Incorrect file: %s/NSIO/%s' % (home, i[1]))
         continue
-    url = 'https://sio.men.gov.pl/dodatki/strefa/index.php?param=Support_download_' + i[0]
-    print(url)
-    req = urllib2.Request(url)
-    req.add_header('User-Agent', 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.8.1.14) Gecko/20080404 Firefox/2.0.0.14')
-    req.add_header('Cookie', cookie)
-    print(req)
-    response = urllib2.urlopen(req)
-    print(response.read())
-    sys.exit()
-
-    subprocess.check_call([
-        'wget',
-        '--load-cookies=my_cookies.txt',
+    url = (
         'https://sio.men.gov.pl/dodatki/strefa/index.php?'
-        'param=Support_download_%s' % i[0],
-        '--output-document=%s/NSIO/new_%s' % (home, i[1])
-    ], stdout=fnull, stderr=subprocess.STDOUT)
+        'param=Support_download_' + i[0]
+    )
+    print(url)
+    page = opener.open(url)
+    with open(os.path.join(home, 'NSIO', 'new_' + i[1]), 'w') as f:
+        f.write(page.read())
     try:
         tree = etree.parse(os.path.join('%s/NSIO/new_%s' % (home, i[1])))
         title_new = tree.xpath('//ss:Row[2]/ss:Cell/ss:Data/text()',
@@ -104,5 +78,3 @@ for i in report_list:
         shutil.copyfile(os.path.join('%s/NSIO/new_%s' % (home, i[1])),
                         os.path.join('%s/NSIO/%s' % (home, i[1])))
         os.remove(os.path.join('%s/NSIO/new_%s' % (home, i[1])))
-os.remove(os.path.join('my_cookies.txt'))
-fnull.close()
