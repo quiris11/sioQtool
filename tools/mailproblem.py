c6a34e6cbe6b78ce6644ba00ed10222542fa29bf
Robert Błaut <listy@blaut.biz>
2016-05-11 13:33:53 +0200

    ...

--- tools/mailproblem.py
+++ tools/mailproblem.py
@@ -10,6 +10,7 @@ def mail_problem(csvf, params):
     import smtplib
     import csv
     import imp
+    import sys
     from email.mime.text import MIMEText
     from email.header import Header
 
@@ -22,33 +23,30 @@ def mail_problem(csvf, params):
 
     def mail(msg, data):
         server = smtplib.SMTP(data.SMTP_SERVER)
-        server.sendmail(data.FROM_ADDRESS, data.TO_ADDRESS, msg.as_string())
+        # server.sendmail(data.FROM_ADDRESS, [row[13], row[14]], msg.as_string())  # Be cafeful !!!
+        # server.sendmail(data.FROM_ADDRESS, data.TO_ADDRESS, msg.as_string())  # Be cafeful !!!
         server.quit()
 
     with open(csvf) as f:
-        for row in csv.reader(f, delimiter=';', quotechar='"'):
-            # print(row)
-            # print(row[0])
+        counter = 0
+        for row in csv.reader(f, delimiter=',', quotechar='"'):
+            counter += 1
+            # if counter == 30:
+            #     sys.exit()
+            if '@' not in row[13]:
+                continue
+            print(row[11], row[12], row[13], row[14])
             content = 'Szanowni Państwo,\n\n' \
-                'Analiza danych zgromadzonych w dwóch systemach informacji ' \
-                'oświatowej (starym SIO i nowym SIO) wykazała istnienie ' \
-                'poważnego ' \
-                'problemu w szkole/placówce o następujących danych ' \
-                'identyfikacyjnych:\n\n' \
-                'REGON: %s\n' \
-                'Nr RSPO: %s\n' \
-                'Nazwa: %s\n\n' \
-                'Problem polega na niezgodności danych pomiędzy dwoma ' \
-                'systemami SIO w następującym obszarze:\n\n' \
-                '\n\n-- \n%s' % (row[4], row[3], row[10], data.SIGNED)
+                'Wszelkie pytania proszę kierować do:\n' \
+                '\n\n-- \n%s' % (row[11], row[12], data.SIGNED)
             msg = MIMEText(content, 'plain', 'utf-8')
-            msg['To'] = data.TO_ADDRESS
+            msg['To'] = row[13]
+            msg['CC'] = row[14]
             msg['From'] = data.FROM_ADDRESS
-            msg['Subject'] = Header('Test - zażółć gęślą jaźń', 'utf-8')
-            print content
-            # print msg.as_string()
+            msg['Subject'] = Header('Dane do logowania w sytemie „Dotacje”',
+                                    'utf-8')
+            # print content
+            mail(msg, data)
 
-            # mail(msg, data)
 if __name__ == '__main__':
-    mail_problem('!critical!/osn_niepoprawne_pole_publicznosc.csv',
-                 'Dropbox/smtp.txt')
+    mail_problem('dotacje.csv', 'Dropbox/smtp.txt')
