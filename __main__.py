6f64c7cf2c892af31ac8ece2b847f7561293859e
Robert Błaut <listy@blaut.biz>
2017-10-10 08:51:28 +0200

    new border dates for new old SIO session - 30/09/2017

--- __main__.py
+++ __main__.py
@@ -37,8 +37,8 @@ XLSNS = {'o': 'urn:schemas-microsoft-com:office:office',
          'x': 'urn:schemas-microsoft-com:office:excel',
          'ss': 'urn:schemas-microsoft-com:office:spreadsheet'}
 
-BORDER_DATE = datetime.strptime('2017-03-31', '%Y-%m-%d')
-BORDER_DATEZ = datetime.strptime('2016-09-01', '%Y-%m-%d')
+BORDER_DATE = datetime.strptime('2017-09-30', '%Y-%m-%d')
+BORDER_DATEZ = datetime.strptime('2016-08-31', '%Y-%m-%d')
 
 parser = argparse.ArgumentParser()
 parser.add_argument('oldpath', nargs='?', default=os.path.join(home, 'OSIO'),
